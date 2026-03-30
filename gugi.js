4d2b0b390f10fbd5e484ce1c85d0445225a567c2
Szathmári László Antal <nemzetiadovamhivatal53@gmail.com>
2026-03-30 22:46:17 +0200

    major changes
    so i almost done the html part of the site what i still need is a backend to get the information from KRETA and then only the css to design it.

--- gugi.js
+++ gugi.js
@@ -4,9 +4,11 @@ const vissza = document.getElementById('vissza');
 
 karalabe.addEventListener('click', () => {
     bal.classList.add('hidden');
+    vissza.classList.add('hid');
 });
 vissza.addEventListener('click', () =>{
     bal.classList.remove('hidden');
+    vissza.classList.remove('hid');
 });
 karalabe.addEventListener('click', () => {
     document.body.classList.add('sidebar');
